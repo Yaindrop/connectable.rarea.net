cdfd3280888f46e6c29adbc274adfedbeef4a796
Yaindrop <yuan_dian@live.com>
2019-10-23 15:56:06 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var updateSize = function () {
+    var w = window,
+        d = document,
+        e = d.documentElement,
+        g = d.getElementsByTagName('body')[0],
+        x = w.innerWidth || e.clientWidth || g.clientWidth;
+    var title = d.getElementsByTagName("title")[0].innerHTML;
+    var head = d.getElementById("head");
+    if (x <= 700) {
+        if (title.length > 15) {
+            console.log("x <= 700");
+            head.style.fontSize = parseInt(150 / title.length) + "vw";
+        }
+    } else {
+        head.style.fontSize = "3rem";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    console.log("here");
+    updateSize();
+});
+
+window.onresize = updateSize;
